888f22b87ce6e9da0eb963c0ea4cbca250ad59bc
JasonLee0905 <101697154+JasonLee0905@users.noreply.github.com>
2022-05-15 18:29:34 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace _0427app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnload = new System.Windows.Forms.Button();
+            this.btnload2 = new System.Windows.Forms.Button();
+            this.btngray = new System.Windows.Forms.Button();
+            this.btngray1 = new System.Windows.Forms.Button();
+            this.btnlighter = new System.Windows.Forms.Button();
+            this.btnMosaic = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btnblur = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::_0427app.Properties.Resources.p3_jpg;
+            this.pictureBox1.Location = new System.Drawing.Point(46, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(366, 485);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(465, 36);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(364, 485);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // btnload
+            // 
+            this.btnload.Font = new System.Drawing.Font("微軟正黑體", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnload.Location = new System.Drawing.Point(46, 553);
+            this.btnload.Name = "btnload";
+            this.btnload.Size = new System.Drawing.Size(353, 118);
+            this.btnload.TabIndex = 2;
+            this.btnload.Text = "載入基本圖片";
+            this.btnload.UseVisualStyleBackColor = true;
+            this.btnload.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnload2
+            // 
+            this.btnload2.Font = new System.Drawing.Font("微軟正黑體", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnload2.Location = new System.Drawing.Point(426, 661);
+            this.btnload2.Name = "btnload2";
+            this.btnload2.Size = new System.Drawing.Size(256, 118);
+            this.btnload2.TabIndex = 3;
+            this.btnload2.Text = "載入圖片";
+            this.btnload2.UseVisualStyleBackColor = true;
+            this.btnload2.Click += new System.EventHandler(this.btnload2_Click);
+            // 
+            // btngray
+            // 
+            this.btngray.Font = new System.Drawing.Font("微軟正黑體", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btngray.Location = new System.Drawing.Point(688, 661);
+            this.btngray.Name = "btngray";
+            this.btngray.Size = new System.Drawing.Size(256, 118);
+            this.btngray.TabIndex = 4;
+            this.btngray.Text = "負片效果";
+            this.btngray.UseVisualStyleBackColor = true;
+            this.btngray.Click += new System.EventHandler(this.btngray_Click);
+            // 
+            // btngray1
+            // 
+            this.btngray1.Font = new System.Drawing.Font("微軟正黑體", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btngray1.Location = new System.Drawing.Point(950, 661);
+            this.btngray1.Name = "btngray1";
+            this.btngray1.Size = new System.Drawing.Size(256, 118);
+            this.btngray1.TabIndex = 5;
+            this.btngray1.Text = "灰階轉換";
+            this.btngray1.UseVisualStyleBackColor = true;
+            this.btngray1.Click += new System.EventHandler(this.btngray1_Click);
+            // 
+            // btnlighter
+            // 
+            this.btnlighter.Font = new System.Drawing.Font("微軟正黑體", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnlighter.Location = new System.Drawing.Point(1212, 661);
+            this.btnlighter.Name = "btnlighter";
+            this.btnlighter.Size = new System.Drawing.Size(256, 118);
+            this.btnlighter.TabIndex = 6;
+            this.btnlighter.Text = "影像變亮";
+            this.btnlighter.UseVisualStyleBackColor = true;
+            this.btnlighter.Click += new System.EventHandler(this.btnlighter_Click);
+            // 
+            // btnMosaic
+            // 
+            this.btnMosaic.Font = new System.Drawing.Font("微軟正黑體", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnMosaic.Location = new System.Drawing.Point(426, 785);
+            this.btnMosaic.Name = "btnMosaic";
+            this.btnMosaic.Size = new System.Drawing.Size(256, 118);
+            this.btnMosaic.TabIndex = 7;
+            this.btnMosaic.Text = "馬賽克";
+            this.btnMosaic.UseVisualStyleBackColor = true;
+            this.btnMosaic.Click += new System.EventHandler(this.btnMosaic_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(884, 36);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(364, 485);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // btnblur
+            // 
+            this.btnblur.Font = new System.Drawing.Font("微軟正黑體", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnblur.Location = new System.Drawing.Point(688, 785);
+            this.btnblur.Name = "btnblur";
+            this.btnblur.Size = new System.Drawing.Size(256, 118);
+            this.btnblur.TabIndex = 9;
+            this.btnblur.Text = "模糊效果";
+            this.btnblur.UseVisualStyleBackColor = true;
+            this.btnblur.Click += new System.EventHandler(this.btnblur_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("微軟正黑體", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn1.Location = new System.Drawing.Point(950, 785);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(256, 118);
+            this.btn1.TabIndex = 10;
+            this.btn1.Text = "上下顛倒";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btnred_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("微軟正黑體", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn2.Location = new System.Drawing.Point(1212, 785);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(256, 118);
+            this.btn2.TabIndex = 11;
+            this.btn2.Text = "黑白變色";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(816, 909);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(256, 118);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "浮雕";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1874, 1086);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btnblur);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.btnMosaic);
+            this.Controls.Add(this.btnlighter);
+            this.Controls.Add(this.btngray1);
+            this.Controls.Add(this.btngray);
+            this.Controls.Add(this.btnload2);
+            this.Controls.Add(this.btnload);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnload;
+        private System.Windows.Forms.Button btnload2;
+        private System.Windows.Forms.Button btngray;
+        private System.Windows.Forms.Button btngray1;
+        private System.Windows.Forms.Button btnlighter;
+        private System.Windows.Forms.Button btnMosaic;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnblur;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace _0427app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Load(@"can help in anything-logo.png");
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnload2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dia = new OpenFileDialog();
+            dia.Title = "Please choose the picture!";
+            dia.Filter = "JPEG files|*.jpg";
+            if (dia.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(dia.FileName);
+            }
+        }
+
+        private void btngray_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = null;
+            int w, h;
+
+            bmp = new Bitmap(pictureBox1.Image);
+            w = bmp.Width;
+            h = bmp.Height;
+            for (int y = 0; y < h; y++)
+            {
+                for (int x = 0; x < w; x++)
+                {
+                    Color p = bmp.GetPixel(x, y);
+                    bmp.SetPixel(x, y, Color.FromArgb(p.A, 255 - p.R, 255 - p.G, 255 - p.B));
+                }
+            }
+            pictureBox2.Image = bmp;
+        }
+
+        private void btngray1_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = null;
+            int w, h;
+
+            bmp = new Bitmap(pictureBox1.Image);
+            w = bmp.Width;
+            h = bmp.Height;
+            for (int y = 0; y < h; y++)
+            {
+                for (int x = 0; x < w; x++)
+                {
+                    Color p = bmp.GetPixel(x, y);
+                    int avg = (int)((0.3 * p.R + p.G * 0.6 + p.B * 0.1));
+                    bmp.SetPixel(x, y, Color.FromArgb(p.A, avg, avg, avg));
+                }
+            }
+            pictureBox2.Image = bmp;
+        }
+
+        private void btnlighter_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = null;
+            int w, h;
+
+            bmp = new Bitmap(pictureBox1.Image);
+            w = bmp.Width;
+            h = bmp.Height;
+            for (int y = 0; y < h; y++)
+            {
+                for (int x = 0; x < w; x++)
+                {
+                    Color p = bmp.GetPixel(x, y);
+                    int r = p.R * 1.8 > 255 ? 255 : (int)(p.R * 1.8);
+                    int g = p.G * 1.8 > 255 ? 255 : (int)(p.G * 1.8);
+                    int b = p.B * 1.8 > 255 ? 255 : (int)(p.B * 1.8);
+                    bmp.SetPixel(x, y, Color.FromArgb(p.A, r, g, b));
+                }
+            }
+            pictureBox2.Image = bmp;
+        }
+
+        private void btnMosaic_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = null;
+            int w, h;
+
+            bmp = new Bitmap(pictureBox1.Image);
+            w = bmp.Width;
+            h = bmp.Height;
+            const int size = 20;
+            for (int y = 0; y < h - size; y += size)
+            {
+                for (int x = 0; x < w - size; x += size)
+                {
+                    Color p = bmp.GetPixel(x, y);
+
+                    for (int i = 0; i < size; i++)
+                    {
+                        for (int j = 0; j < size; j++)
+                        {
+                            bmp.SetPixel(x + i, y + j, p);
+                        }
+                    }
+                }
+            }
+            pictureBox2.Image = bmp;
+        }
+
+        private void btnblur_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = null;
+            int w, h;
+
+            bmp = new Bitmap(pictureBox1.Image);
+            w = bmp.Width;
+            h = bmp.Height;
+            for (int y = 0; y < h; y++)
+            {
+                for (int x = 0; x < w; x++)
+                {
+                    Color p = bmp.GetPixel(x, y);
+                    int avg = (int)(p.R * 0.3 + p.G * 0.6 + p.B * 0.1);
+                    bmp.SetPixel(x, y, Color.FromArgb(p.A, avg, avg, avg));
+                }
+            }
+            pictureBox2.Image = bmp;
+            Bitmap bmp1 = new Bitmap(pictureBox2.Image);
+            for (int y = 10; y < h - 10; y++)
+            {
+                for (int x = 10; x < w - 10; x++)
+                {
+                    int total = 0;
+                    int avg = 0;
+                    int a = 0;
+                    for (int i = 0; i < 10; i++)
+                    {
+                        for (int j = 0; j < 10; j++)
+                        {
+                            Color p = bmp1.GetPixel(x + i, y + j);
+                            a = p.A;
+                            total += p.R;
+                        }
+                        avg = (int)(total / 100);
+                    }
+                    bmp1.SetPixel(x, y, Color.FromArgb(a, avg, avg, avg));
+                }
+            }
+            pictureBox1.Image = bmp1;
+        }
+
+        private void btnred_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = null;
+            int w, h;
+
+            bmp = new Bitmap(pictureBox1.Image);
+            w = bmp.Width;
+            h = bmp.Height;
+            for (int y = 0; y < h; y++)
+            {
+                for (int x = 0; x < w; x++)
+                {
+                    Color p = bmp.GetPixel(x, y);
+                    bmp.SetPixel(x, y, Color.FromArgb(p.A, p.R, p.G, p.B));
+                }
+            }
+            bmp.RotateFlip(RotateFlipType.Rotate180FlipX);
+            pictureBox2.Image = bmp;
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = null;
+            int w, h;
+
+            bmp = new Bitmap(pictureBox1.Image);
+            w = bmp.Width;
+            h = bmp.Height;
+            for (int y = 0; y < h; y++)
+            {
+                for (int x = 0; x < w; x++)
+                {
+                    Color p = bmp.GetPixel(x, y);
+                    int r = p.R;
+                    int g = p.G;
+                    int b = p.B;
+                    if ((r + g + b) / 3 >= 127)
+                        bmp.SetPixel(x, y, Color.FromArgb(p.A, 255, 255, 255));
+                    else
+                        bmp.SetPixel(x, y, Color.FromArgb(p.A, 0, 0, 0));
+                }
+            }
+            pictureBox2.Image = bmp;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int Height = this.pictureBox1.Image.Height;
+            int Width = this.pictureBox1.Image.Width;
+            Bitmap newBitmap = new Bitmap(Width, Height);
+            Bitmap oldBitmap = (Bitmap)this.pictureBox1.Image;
+            Color pixel1, pixel2;
+            for (int x = 0; x < Width - 1; x++)
+            {
+                for (int y = 0; y < Height - 1; y++)
+                {
+                    int r = 0, g = 0, b = 0;
+                    pixel1 = oldBitmap.GetPixel(x, y);
+                    pixel2 = oldBitmap.GetPixel(x + 1, y + 1);
+                    r = Math.Abs(pixel1.R - pixel2.R + 128);
+                    g = Math.Abs(pixel1.G - pixel2.G + 128);
+                    b = Math.Abs(pixel1.B - pixel2.B + 128);
+                    if (r > 255)
+                        r = 255;
+                    if (r < 0)
+                        r = 0;
+                    if (g > 255)
+                        g = 255;
+                    if (g < 0)
+                        g = 0;
+                    if (b > 255)
+                        b = 255;
+                    if (b < 0)
+                        b = 0;
+                    newBitmap.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+            this.pictureBox2.Image = newBitmap;
+        }
+    }
+}
